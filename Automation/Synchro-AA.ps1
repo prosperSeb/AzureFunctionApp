18827b5c91bf054cd23d74cac54ba4f7591882a0
prosperSeb <50331503+prosperSeb@users.noreply.github.com>
2021-10-12 10:48:27 +0200

    Update Synchro-AA.ps1

--- Automation/Synchro-AA.ps1
+++ Automation/Synchro-AA.ps1
@@ -3,19 +3,22 @@
 
 #Param
 $subid  = "4f975edb-da7f-4245-84a3-c1c97b8b9ef3"
-$aaName = "AA-Discovery-01-Fix"
 $aaRG   = "Discovery-v2.3-Fix"
 
 Set-AzContext -Subscription $subid
 
 $scriptPath = @("./Core")
 
-foreach ($s in $scriptPath){
+$aaList = @()
+$aaList = Get-AzAutomationAccount -ResourceGroupName $aaRG
+
+foreach ($aa in $aaList){
+    foreach ($s in $scriptPath){
 
     $psScript = Get-ChildItem -path $s *.ps1
     
     foreach ($ps in $psScript) {
-        Import-AzAutomationRunbook -AutomationAccountName $aaName `
+        Import-AzAutomationRunbook -AutomationAccountName $aa `
                             -ResourceGroupName $aaRG `
                             -Name $(($ps.name).TrimEnd('.ps1')) `
                             -Path $ps.FullName `
@@ -25,5 +28,8 @@ foreach ($s in $scriptPath){
                             -Force
     }   
 
+    }
 }
+
+
 
