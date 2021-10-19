4505a99b310c4444731cc1018ff458034d7c2ba1
prosperSeb <50331503+prosperSeb@users.noreply.github.com>
2021-10-19 11:12:41 +0200

    Update __init___.py

--- Core-Function/HttpTrigger1/__init___.py
+++ Core-Function/HttpTrigger1/__init___.py
@@ -2,6 +2,7 @@ import logging
 
 import azure.functions as func
 
+#Test
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
